193caa1d74cb23c18059a5734aa4b08877fd3b3e
Priyansh <pradadiya667@gmail.com>
2019-05-10 12:26:51 -0400

    Added burger.js into model by adding an object burger

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.selectAll("*", "burgers", function(result) {
+      cb(res);
+    });
+  },
+  insert: function(set, cb) {
+    orm.insertOne("burgers", set, function(result) {
+      cb(result);
+    });
+  },
+  update: function(set, where, cb) {
+    orm.updateOne("burgers", set, where, function(result) {
+      cb(result);
+    });
+  }
+};
+
+module.exports = burgers;
