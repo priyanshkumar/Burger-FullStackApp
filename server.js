d447386aa26d8ede1c7deaabd4e9ccf866dd9747
Priyansh <pradadiya667@gmail.com>
2019-05-10 11:02:38 -0400

    new server up and running

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var path = require("path");
+
+var port = proces.env.port || 3000;
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(port, function() {
+  console.log(`server running on ${port}`);
+});
